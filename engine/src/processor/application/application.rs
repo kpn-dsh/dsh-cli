5e97cdc16b3af582a894bdcace458c50397a41ce
wilbert <wilbert.schelvis@kpn.com>
2024-07-15 15:35:02 +0200

    Fixed status method

--- engine/src/processor/application/application.rs
+++ engine/src/processor/application/application.rs
@@ -1,16 +1,16 @@
 #![allow(clippy::module_inception)]
 
-use std::collections::HashMap;
-
 use async_trait::async_trait;
+use dsh_rest_api_client::Error::UnexpectedResponse;
+use log::error;
 
 use crate::processor::application::application_config::{ApplicationConfig, ProfileConfig};
-use crate::processor::application::converters::into_api_application;
+use crate::processor::application::converters::{into_api_application, TemplateMapping};
+use crate::processor::application::TargetClientFactory;
 use crate::processor::processor::{Processor, ProcessorDeployParameters, ProcessorIdentifier, ProcessorStatus};
 use crate::processor::processor_config::PlaceHolder;
 use crate::processor::processor_descriptor::ProcessorDescriptor;
 use crate::processor::ProcessorType;
-use crate::TargetClientFactory;
 
 pub struct ApplicationImpl<'a> {
   processor_identifier: ProcessorIdentifier,
@@ -21,9 +21,9 @@ pub struct ApplicationImpl<'a> {
 
 impl<'a> ApplicationImpl<'a> {
   pub fn create(application_config: ApplicationConfig, client_factory: &'a TargetClientFactory) -> Result<Self, String> {
-    let template_mapping: HashMap<PlaceHolder, &str> = HashMap::from([(PlaceHolder::TENANT, client_factory.tenant.as_str()), (PlaceHolder::USER, client_factory.user.as_str())]);
+    let template_mapping = TemplateMapping::from(client_factory);
     Ok(ApplicationImpl {
-      processor_identifier: ProcessorIdentifier { processor_type: ProcessorType::Application, name: application_config.application_name.clone() },
+      processor_identifier: ProcessorIdentifier { processor_type: ProcessorType::Application, id: application_config.application_id.clone() },
       processor_descriptor: ProcessorDescriptor::from((&application_config, &template_mapping)),
       config: application_config,
       target_client_factory: client_factory,
@@ -33,9 +33,9 @@ impl<'a> ApplicationImpl<'a> {
 
 #[async_trait]
 impl Processor for ApplicationImpl<'_> {
-  async fn deploy(&self, processor_instance_name: &str, parameters: &ProcessorDeployParameters) -> Result<(), String> {
-    let profile: ProfileConfig = match parameters.profile_name {
-      Some(pn) => match self.config.application.profiles.get(pn) {
+  async fn deploy(&self, instance_id: &str, parameters: &ProcessorDeployParameters) -> Result<(), String> {
+    let profile: ProfileConfig = match parameters.profile_id {
+      Some(pn) => match self.config.application.profiles.iter().find(|p| p.id == pn) {
         Some(p) => p.clone(),
         None => return Err(format!("profile {} is not defined", pn)),
       },
@@ -43,19 +43,19 @@ impl Processor for ApplicationImpl<'_> {
         if self.config.application.profiles.is_empty() {
           return Err("no default profile defined".to_string());
         } else if self.config.application.profiles.len() == 1 {
-          self.config.application.profiles.iter().nth(1).map(|p| p.1.clone()).unwrap()
+          self.config.application.profiles.get(1).cloned().unwrap()
         } else {
           return Err("unable to select profile".to_string());
         }
       }
     };
     let target_client = self.target_client_factory.get().await?;
-    let template_mapping: HashMap<PlaceHolder, &str> =
-      HashMap::from([(PlaceHolder::INSTANCE, processor_instance_name), (PlaceHolder::TENANT, target_client.tenant.as_str()), (PlaceHolder::USER, target_client.user)]);
+    let mut template_mapping: TemplateMapping = TemplateMapping::from(self.target_client_factory);
+    template_mapping.insert(PlaceHolder::Instance, instance_id.to_string());
     let api_application = into_api_application(&self.config, parameters, &profile, target_client.user.clone(), &template_mapping)?;
     match target_client
       .client
-      .application_put_by_tenant_application_by_appid_configuration(target_client.tenant, processor_instance_name, &target_client.token, &api_application)
+      .application_put_by_tenant_application_by_appid_configuration(target_client.tenant, instance_id, &target_client.token, &api_application)
       .await
     {
       Ok(_) => Ok(()),
@@ -71,23 +71,27 @@ impl Processor for ApplicationImpl<'_> {
     &self.processor_identifier
   }
 
-  fn name(&self) -> &str {
-    &self.processor_identifier.name
+  fn id(&self) -> &str {
+    &self.processor_identifier.id
+  }
+
+  fn label(&self) -> &str {
+    &self.processor_descriptor.label
   }
 
   fn processor_type(&self) -> ProcessorType {
     ProcessorType::Application
   }
 
-  async fn start(&self, _processor_name: &str) -> Result<String, String> {
-    todo!()
+  async fn start(&self, _instance_id: &str) -> Result<String, String> {
+    Err("start method not yet implemented".to_string())
   }
 
-  async fn status(&self, instance_name: &str) -> Result<ProcessorStatus, String> {
+  async fn status(&self, instance_id: &str) -> Result<ProcessorStatus, String> {
     let target_client = self.target_client_factory.get().await?;
     match target_client
       .client
-      .application_get_by_tenant_application_by_appid_status(target_client.tenant, instance_name, &target_client.token)
+      .application_get_by_tenant_application_by_appid_status(target_client.tenant, instance_id, &target_client.token)
       .await
     {
       Ok(r) => {
@@ -97,19 +101,30 @@ impl Processor for ApplicationImpl<'_> {
           Ok(ProcessorStatus { up: false })
         }
       }
-      Err(e) => Err(e.to_string()),
+      Err(UnexpectedResponse(e)) => {
+        if e.status() == 404 {
+          Ok(ProcessorStatus { up: false })
+        } else {
+          error!("unexpected response on get status request: {:?}", e);
+          Err("unexpected response on get status request".to_string())
+        }
+      }
+      Err(e) => {
+        error!("unexpected error on get status request: {:?}", e);
+        Err("unexpected error on get status request".to_string())
+      }
     }
   }
 
-  async fn stop(&self, _processor_name: &str) -> Result<String, String> {
-    todo!()
+  async fn stop(&self, _instance_id: &str) -> Result<String, String> {
+    Err("stop method not yet implemented".to_string())
   }
 
-  async fn undeploy(&self, instance_name: &str) -> Result<(), String> {
+  async fn undeploy(&self, instance_id: &str) -> Result<(), String> {
     let target_client = self.target_client_factory.get().await?;
     match target_client
       .client
-      .application_delete_by_tenant_application_by_appid_configuration(target_client.tenant, instance_name, &target_client.token)
+      .application_delete_by_tenant_application_by_appid_configuration(target_client.tenant, instance_id, &target_client.token)
       .await
     {
       Ok(_) => Ok(()),
