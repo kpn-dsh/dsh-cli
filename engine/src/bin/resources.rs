2d734fc5c33e3c22082fe44265df79970548e51f
wilbert <wilbert.schelvis@kpn.com>
2024-07-20 18:09:24 +0200

    Refactored for better example

--- engine/src/bin/resources.rs
+++ engine/src/bin/resources.rs
@@ -1,7 +1,8 @@
-use trifonius_engine::resource::resource_registry::ResourceRegistry;
+use trifonius_engine::processor::processor_registry::ProcessorRegistry;
 
 fn main() {
-  let resource_registry = ResourceRegistry::default();
+  let processor_registry = ProcessorRegistry::default();
+  let resource_registry = processor_registry.resource_registry();
   let resource_descriptors = resource_registry.resource_descriptors();
   println!("{}", serde_json::to_string_pretty(&resource_descriptors).unwrap());
 }
