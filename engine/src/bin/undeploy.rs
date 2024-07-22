b5d5c53d463298d7defcc431139ffc7db97762b3
wilbert <wilbert.schelvis@kpn.com>
2024-07-22 11:32:42 +0200

    Suppressed logging

--- engine/src/bin/undeploy.rs
+++ engine/src/bin/undeploy.rs
@@ -5,7 +5,6 @@ const SERVICE_ID: &str = "test-0-0-2";
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
-  env_logger::init();
   let processor_registry = ProcessorRegistry::default();
   let dsh_service = processor_registry.processor(ProcessorType::DshService, "consentfilter").ok_or("")?;
   println!("{}", dsh_service.undeploy(SERVICE_ID).await?);
