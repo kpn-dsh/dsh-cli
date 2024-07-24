f18e6a22141316c1935e5c45a760a569243b0d83
wilbert <wilbert.schelvis@kpn.com>
2024-07-24 21:38:17 +0200

    Added examples to the doc comments

--- engine/src/macros.rs
+++ engine/src/macros.rs
@@ -12,18 +12,22 @@ macro_rules! identifier {
     pub struct $name(String);
 
     impl $name {
-      #[doc = concat!("# Create a new `", stringify!($name), "`")]
-      #[doc = ""]
-      #[doc = concat!("This static method is used to create a new `", stringify!($name), "` instance from the provided `&str`, representing a ", $label, ".")]
-      #[doc = concat!("The `id` argument will be validated against the regular expression `", $regex, "`. When the validation fails, the function will panic.")]
-      #[doc = concat!("When you need a safe constructor, use one of the [`", stringify!($name), "::try_from()`] methods.")]
-      #[doc = ""]
+      #[doc = concat!("# Create new `", stringify!($name), "`")]
+      #[doc = concat!("This static factory function is used to create a new `", stringify!($name), "` instance from the provided string slice, representing a ", $label, ".")]
+      #[doc = concat!("The `id` argument will be validated against the regular expression `", $regex, "`.")]
+      #[doc = "If validation fails, the function will panic."]
+      #[doc = concat!("If you need a safe factory function, use one of the [`", stringify!($name), "::try_from()`] methods, which will return a `Result<", stringify!($name), ", String>`.")]
+      #[doc = "## Examples"]
+      #[doc = concat!("Create a `", stringify!($name), "` instance from a string slice:")]
+      #[doc = "```"]
+      #[doc = concat!("let id = ", stringify!($name), "::new(\"abcdefgh\");")]
+      #[doc = concat!("println!(\"", $label, " is {}\", id);")]
+      #[doc = "```"]
       #[doc = "## Parameters"]
       #[doc = concat!("* `id` - the ", $label, ".")]
-      #[doc = ""]
       #[doc = "## Returns"]
       #[doc = concat!("* The created `", stringify!($name), "`, when validation was successful.")]
-      #[doc = concat!("* The function panics when the validation was not successful.")]
+      #[doc = "* The function panics when the validation was not successful."]
       pub fn new(id: &str) -> Self {
         if Self::is_valid(id) {
           Self(id.to_string())
@@ -32,27 +36,36 @@ macro_rules! identifier {
         }
       }
 
-      #[doc = concat!("# Validates a ", $label)]
-      #[doc = ""]
-      #[doc = concat!("This static method is used to check whether the provided `&str` is a valid ", $label, ".")]
-      #[doc = concat!("The `id` argument will be validated against the regular expression `", $regex, "`. When the validation is successful, the function will return `true`, else it returns `false`.")]
-      #[doc = concat!("If `is_valid()` returns `true`, the `", stringify!($name), "::new()` function will never panic and can be safely used.")]
-      #[doc = ""]
+      #[doc = concat!("# Validate ", $label)]
+      #[doc = concat!("This static function is used to check whether the provided string slice is a valid ", $label, ".")]
+      #[doc = concat!("The `id` argument will be validated against the regular expression `", $regex, "`.")]
+      #[doc = "When `id` is valid, the function will return `true`, else it will return `false`."]
+      #[doc = concat!("If `is_valid(id)` returns `true`, [`", stringify!($name), "::new(id)`] will never panic and can be used safely.")]
+      #[doc = "## Examples"]
+      #[doc = concat!("Validate a ", $label, " string slice:")]
+      #[doc = "```"]
+      #[doc = concat!("assert!(", stringify!($name), "::is_valid(\"abcdefgh\"));")]
+      #[doc = concat!("assert!(!", stringify!($name), "::is_valid(\"12345678\"));")]
+      #[doc = "```"]
       #[doc = "## Parameters"]
       #[doc = concat!("* `id` - the ", $label, ".")]
-      #[doc = ""]
       #[doc = "## Returns"]
       #[doc = concat!("* `true` - when `id` matches the regular expression `", $regex, "`,")]
-      #[doc = concat!("* `false` - otherwise.")]
+      #[doc = "* `false` - otherwise."]
       pub fn is_valid(id: &str) -> bool {
         Self::regex().is_match(id)
       }
 
       #[doc = "# Returns the regular expression"]
-      #[doc = ""]
-      #[doc = concat!("This static method will return a reference to the regular expression used by this `", stringify!($name), "` to validate a ", $label, " string.")]
+      #[doc = concat!("This static function will return a reference to the regular expression used by this `", stringify!($name), "` to validate ", $label, " strings.")]
       #[doc = concat!("The textual representation of the regular expression is `", $regex, "`.")]
-      #[doc = ""]
+      #[doc = "## Examples"]
+      #[doc = concat!("Validate a ", $label, " using the regular expression:")]
+      #[doc = "```"]
+      #[doc = concat!("let regex: &'static Regex = ", stringify!($name), "::regex();")]
+      #[doc = concat!("assert!(regex.is_match(\"abcdefgh\"));")]
+      #[doc = concat!("assert!(!regex.is_match(\"12345678\"));")]
+      #[doc = "```"]
       #[doc = "## Returns"]
       #[doc = concat!("The regular expression used by this `", stringify!($name), "`.")]
       pub fn regex() -> &'static regex::Regex {
