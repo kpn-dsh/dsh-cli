df35387cba424866e8291c271da1d7c3de93e00f
wilbert <wilbert.schelvis@kpn.com>
2024-09-19 17:05:58 +0200

    Proper implementation

--- tcli/src/formatters/app.rs
+++ tcli/src/formatters/app.rs
@@ -1,6 +1,7 @@
-use trifonius_dsh_api::types::AppCatalogApp;
-
 use crate::formatters::formatter::{Label, SubjectFormatter};
+use serde_json::de::from_str;
+use std::collections::HashMap;
+use trifonius_dsh_api::types::AppCatalogApp;
 
 #[derive(Eq, Hash, PartialEq)]
 pub(crate) enum AppCatalogAppLabel {
@@ -20,9 +21,26 @@ impl Label for AppCatalogAppLabel {
 }
 
 impl SubjectFormatter<AppCatalogAppLabel> for AppCatalogApp {
-  fn value(&self, column: &AppCatalogAppLabel, target_id: &str) -> String {
-    match column {
-      AppCatalogAppLabel::Configuration => self.configuration.clone().unwrap_or_default().to_string(),
+  fn value(&self, label: &AppCatalogAppLabel, target_id: &str) -> String {
+    match label {
+      AppCatalogAppLabel::Configuration => match &self.configuration {
+        Some(configuration) => match from_str::<HashMap<String, String>>(configuration) {
+          Ok(map) => {
+            let mut keys: Vec<String> = map
+              .keys()
+              .filter_map(|key| if !key.starts_with("@") { Some(key.to_string()) } else { None })
+              .collect();
+            keys.sort();
+            keys
+              .iter()
+              .map(|key| format!("{} : {}", key, map.get(key).map(|v| v.to_string()).unwrap_or("".to_string())))
+              .collect::<Vec<String>>()
+              .join("\n")
+          }
+          Err(_) => "error".to_string(),
+        },
+        None => "empty".to_string(),
+      },
       AppCatalogAppLabel::ManifestUrl => self.manifest_urn.clone(),
       AppCatalogAppLabel::Target => target_id.to_string(),
     }
