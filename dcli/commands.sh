af7c35a55bfe12594bff64d137bbc80b9de6d481
wilbert <wilbert.schelvis@kpn.com>
2024-10-28 19:23:53 +0100

    Added new tests

--- dcli/commands.sh
+++ dcli/commands.sh
@@ -11,137 +11,272 @@ export APPLICATION_UNDER_TEST=cmd
 export BUCKET_UNDER_TEST=cpr
 export CERTIFICATE_UNDER_TEST=broker-kafka-proxy-certificate
 export ENV_VALUE_UNDER_TEST=info
+export ENV_VALUE_UNDER_TEST_REGEX="^info$"
+export IMAGE_UNDER_TEST=registry:eavesdropper:0.9.3
+export IMAGE_UNDER_TEST_REGEX=registry
 export PROXY_UNDER_TEST=broker
 export SECRET_UNDER_TEST=boss-account-ids
 export TOPIC_UNDER_TEST=reference-implementation-compliant
 export VHOST_UNDER_TEST=greenbox-dev
 export VOLUME_UNDER_TEST=github-action-runner-home
 
-dcli -vvv app list
-dcli -vvv app list --all
-dcli -vvv app list --status
-dcli -vvv app list --configuration
-dcli -vvv app list --ids
-dcli -vvv app l
-dcli -vvv apps
-dcli -vvv app show $APP_UNDER_TEST
-dcli -vvv app show $APP_UNDER_TEST --all
+export VERBOSITY="-vvv"
 
-dcli -vvv application diff cmd
-dcli -vvv application list
-dcli -vvv application list --all
-dcli -vvv application list --status
-dcli -vvv application list --configuration
-dcli -vvv application list --ids
-dcli -vvv application list --tasks
-dcli -vvv application list --started
-dcli -vvv application list --stopped
-dcli -vvv application l
-dcli -vvv a l
-dcli -vvv applications
-dcli -vvv as
-dcli -vvv application show $APPLICATION_UNDER_TEST
-dcli -vvv application show $APPLICATION_UNDER_TEST --all
-dcli -vvv application show $APPLICATION_UNDER_TEST --status
-dcli -vvv application show $APPLICATION_UNDER_TEST --configuration
-dcli -vvv application show $APPLICATION_UNDER_TEST --tasks
+dcli $VERBOSITY app list
+echo
+dcli $VERBOSITY app list --all
+echo
+dcli $VERBOSITY app list --status
+echo
+dcli $VERBOSITY app list --configuration
+echo
+dcli $VERBOSITY app list --ids
+echo
+dcli $VERBOSITY app l
+echo
+dcli $VERBOSITY apps
+echo
+dcli $VERBOSITY app show $APP_UNDER_TEST
+echo
+dcli $VERBOSITY app show $APP_UNDER_TEST --all
+echo
 
-dcli -vvv bucket list
-dcli -vvv bucket list --all
-dcli -vvv bucket list --status
-dcli -vvv bucket list --configuration
-dcli -vvv bucket list --ids
-dcli -vvv bucket l
-dcli -vvv b l
-dcli -vvv buckets
-dcli -vvv bs
-dcli -vvv bucket show $BUCKET_UNDER_TEST
-dcli -vvv bucket show $BUCKET_UNDER_TEST --all
-dcli -vvv bucket show $BUCKET_UNDER_TEST --status
+dcli $VERBOSITY application diff cmd
+echo
+dcli $VERBOSITY application list
+echo
+dcli $VERBOSITY application list --all
+echo
+dcli $VERBOSITY application list --status
+echo
+dcli $VERBOSITY application list --configuration
+echo
+dcli $VERBOSITY application list --ids
+echo
+dcli $VERBOSITY application list --tasks
+echo
+dcli $VERBOSITY application list --started
+echo
+dcli $VERBOSITY application list --stopped
+echo
+dcli $VERBOSITY application l
+echo
+dcli $VERBOSITY a l
+echo
+dcli $VERBOSITY applications
+echo
+dcli $VERBOSITY as
+echo
+dcli $VERBOSITY application show $APPLICATION_UNDER_TEST
+echo
+dcli $VERBOSITY application show $APPLICATION_UNDER_TEST --all
+echo
+dcli $VERBOSITY application show $APPLICATION_UNDER_TEST --status
+echo
+dcli $VERBOSITY application show $APPLICATION_UNDER_TEST --configuration
+echo
+dcli $VERBOSITY application show $APPLICATION_UNDER_TEST --tasks
+echo
 
-dcli -vvv certificate list
-dcli -vvv certificate list --all
-dcli -vvv certificate list --status
-dcli -vvv certificate list --configuration
-dcli -vvv certificate list --ids
-dcli -vvv certificate list --usage
-dcli -vvv certificate l
-dcli -vvv c l
-dcli -vvv certificates
-dcli -vvv cs
-dcli -vvv certificate show $CERTIFICATE_UNDER_TEST
-dcli -vvv certificate show $CERTIFICATE_UNDER_TEST --all
-dcli -vvv certificate show $CERTIFICATE_UNDER_TEST --status
-dcli -vvv certificate show $CERTIFICATE_UNDER_TEST --usage
+dcli $VERBOSITY bucket list
+echo
+dcli $VERBOSITY bucket list --all
+echo
+dcli $VERBOSITY bucket list --status
+echo
+dcli $VERBOSITY bucket list --configuration
+echo
+dcli $VERBOSITY bucket list --ids
+echo
+dcli $VERBOSITY bucket l
+echo
+dcli $VERBOSITY b l
+echo
+dcli $VERBOSITY buckets
+echo
+dcli $VERBOSITY bs
+echo
+dcli $VERBOSITY bucket show $BUCKET_UNDER_TEST
+echo
+dcli $VERBOSITY bucket show $BUCKET_UNDER_TEST --all
+echo
+dcli $VERBOSITY bucket show $BUCKET_UNDER_TEST --status
+echo
 
-dcli -vvv env find $ENV_VALUE_UNDER_TEST
-dcli -vvv env find $ENV_VALUE_UNDER_TEST --app
-dcli -vvv env find $ENV_VALUE_UNDER_TEST --application
+dcli $VERBOSITY certificate list
+echo
+dcli $VERBOSITY certificate list --all
+echo
+dcli $VERBOSITY certificate list --status
+echo
+dcli $VERBOSITY certificate list --configuration
+echo
+dcli $VERBOSITY certificate list --ids
+echo
+dcli $VERBOSITY certificate list --usage
+echo
+dcli $VERBOSITY certificate l
+echo
+dcli $VERBOSITY c l
+echo
+dcli $VERBOSITY certificates
+echo
+dcli $VERBOSITY cs
+echo
+dcli $VERBOSITY certificate show $CERTIFICATE_UNDER_TEST
+echo
+dcli $VERBOSITY certificate show $CERTIFICATE_UNDER_TEST --all
+echo
+dcli $VERBOSITY certificate show $CERTIFICATE_UNDER_TEST --status
+echo
+dcli $VERBOSITY certificate show $CERTIFICATE_UNDER_TEST --usage
+echo
 
-dcli -vvv manifest list
-dcli -vvv manifest list --all
-dcli -vvv manifest list --configuration
-dcli -vvv manifest list --ids
+dcli $VERBOSITY env find $ENV_VALUE_UNDER_TEST
+echo
+dcli $VERBOSITY env find $ENV_VALUE_UNDER_TEST_REGEX --regex
+echo
+dcli $VERBOSITY env find $ENV_VALUE_UNDER_TEST --app
+echo
+dcli $VERBOSITY env find $ENV_VALUE_UNDER_TEST_REGEX --app --regex
+echo
+dcli $VERBOSITY env find $ENV_VALUE_UNDER_TEST --application
+echo
+dcli $VERBOSITY env find $ENV_VALUE_UNDER_TEST_REGEX --application --regex
+echo
 
-dcli -vvv proxy list
-dcli -vvv proxy list --all
-dcli -vvv proxy list --ids
-dcli -vvv proxy l
-dcli -vvv proxys
-dcli -vvv proxy show $PROXY_UNDER_TEST
-dcli -vvv proxy show $PROXY_UNDER_TEST --configuration
+dcli $VERBOSITY image find $IMAGE_UNDER_TEST
+echo
+dcli $VERBOSITY image find $IMAGE_UNDER_TEST_REGEX --regex
+echo
+dcli $VERBOSITY image find $IMAGE_UNDER_TEST --app
+echo
+dcli $VERBOSITY image find $IMAGE_UNDER_TEST_REGEX --app --regex
+echo
+dcli $VERBOSITY image find $IMAGE_UNDER_TEST --application
+echo
+dcli $VERBOSITY image find $IMAGE_UNDER_TEST_REGEX --application --regex
+echo
 
-dcli -vvv secret list
-dcli -vvv secret list --all
-dcli -vvv secret list --status
-dcli -vvv secret list --ids
-dcli -vvv secret list --usage
-dcli -vvv secret l
-dcli -vvv s l
-dcli -vvv secrets
-dcli -vvv ss
-dcli -vvv secret show $SECRET_UNDER_TEST
-dcli -vvv secret show $SECRET_UNDER_TEST --status
-dcli -vvv secret show $SECRET_UNDER_TEST --usage
-dcli -vvv secret show $SECRET_UNDER_TEST --value
+dcli $VERBOSITY manifest list
+echo
+dcli $VERBOSITY manifest list --all
+echo
+dcli $VERBOSITY manifest list --configuration
+echo
+dcli $VERBOSITY manifest list --ids
+echo
 
-dcli -vvv topic list
-dcli -vvv topic list --status
-dcli -vvv topic list --configuration
-dcli -vvv topic list --ids
-dcli -vvv topic list --usage
-dcli -vvv topic show $TOPIC_UNDER_TEST
-dcli -vvv topic show $TOPIC_UNDER_TEST --status
-dcli -vvv topic show $TOPIC_UNDER_TEST --configuration
-dcli -vvv topic show $TOPIC_UNDER_TEST --usage
-dcli -vvv topic show $TOPIC_UNDER_TEST --properties
+dcli $VERBOSITY proxy list
+echo
+dcli $VERBOSITY proxy list --all
+echo
+dcli $VERBOSITY proxy list --ids
+echo
+dcli $VERBOSITY proxy l
+echo
+dcli $VERBOSITY proxys
+echo
+dcli $VERBOSITY proxy show $PROXY_UNDER_TEST
+echo
+dcli $VERBOSITY proxy show $PROXY_UNDER_TEST --configuration
+echo
 
-dcli -vvv vhost list
-dcli -vvv vhost list --usage
-dcli -vvv vhost l
-dcli -vvv v l
-dcli -vvv vhosts
-dcli -vvv vs
-dcli -vvv vhost show $VHOST_UNDER_TEST
-dcli -vvv vhost show $VHOST_UNDER_TEST --usage
+dcli $VERBOSITY secret list
+echo
+dcli $VERBOSITY secret list --all
+echo
+dcli $VERBOSITY secret list --status
+echo
+dcli $VERBOSITY secret list --ids
+echo
+dcli $VERBOSITY secret list --usage
+echo
+dcli $VERBOSITY secret l
+echo
+dcli $VERBOSITY s l
+echo
+dcli $VERBOSITY secrets
+echo
+dcli $VERBOSITY ss
+echo
+dcli $VERBOSITY secret show $SECRET_UNDER_TEST
+echo
+dcli $VERBOSITY secret show $SECRET_UNDER_TEST --status
+echo
+dcli $VERBOSITY secret show $SECRET_UNDER_TEST --usage
+echo
+dcli $VERBOSITY secret show $SECRET_UNDER_TEST --value
+echo
 
-dcli -vvv volume list
-dcli -vvv volume list --all
-dcli -vvv volume list --status
-dcli -vvv volume list --configuration
-dcli -vvv volume list --ids
-dcli -vvv volume list --usage
-dcli -vvv volume list --usage --app
-dcli -vvv volume list --usage --application
-dcli -vvv volume show $VOLUME_UNDER_TEST
-dcli -vvv volume show $VOLUME_UNDER_TEST --all
-dcli -vvv volume show $VOLUME_UNDER_TEST --status
-dcli -vvv volume show $VOLUME_UNDER_TEST --configuration
-dcli -vvv volume show $VOLUME_UNDER_TEST --usage
+dcli $VERBOSITY topic list
+echo
+dcli $VERBOSITY topic list --status
+echo
+dcli $VERBOSITY topic list --configuration
+echo
+dcli $VERBOSITY topic list --ids
+echo
+dcli $VERBOSITY topic list --usage
+echo
+dcli $VERBOSITY topic show $TOPIC_UNDER_TEST
+echo
+dcli $VERBOSITY topic show $TOPIC_UNDER_TEST --status
+echo
+dcli $VERBOSITY topic show $TOPIC_UNDER_TEST --configuration
+echo
+dcli $VERBOSITY topic show $TOPIC_UNDER_TEST --usage
+echo
+dcli $VERBOSITY topic show $TOPIC_UNDER_TEST --properties
+echo
 
-##dcli -vvv proxy delete
-##dcli -vvv proxy update
-##dcli -vvv secret create
-##dcli -vvv secret delete
-##dcli -vvv volume create
-##dcli -vvv volume delete
+dcli $VERBOSITY vhost list
+echo
+dcli $VERBOSITY vhost list --usage
+echo
+dcli $VERBOSITY vhost l
+echo
+dcli $VERBOSITY v l
+echo
+dcli $VERBOSITY vhosts
+echo
+dcli $VERBOSITY vs
+echo
+dcli $VERBOSITY vhost show $VHOST_UNDER_TEST
+echo
+dcli $VERBOSITY vhost show $VHOST_UNDER_TEST --usage
+echo
+
+dcli $VERBOSITY volume list
+echo
+dcli $VERBOSITY volume list --all
+echo
+dcli $VERBOSITY volume list --status
+echo
+dcli $VERBOSITY volume list --configuration
+echo
+dcli $VERBOSITY volume list --ids
+echo
+dcli $VERBOSITY volume list --usage
+echo
+dcli $VERBOSITY volume list --usage --app
+echo
+dcli $VERBOSITY volume list --usage --application
+echo
+dcli $VERBOSITY volume show $VOLUME_UNDER_TEST
+echo
+dcli $VERBOSITY volume show $VOLUME_UNDER_TEST --all
+echo
+dcli $VERBOSITY volume show $VOLUME_UNDER_TEST --status
+echo
+dcli $VERBOSITY volume show $VOLUME_UNDER_TEST --configuration
+echo
+dcli $VERBOSITY volume show $VOLUME_UNDER_TEST --usage
+echo
+
+##dcli $VERBOSITY proxy delete
+##dcli $VERBOSITY proxy update
+##dcli $VERBOSITY secret create
+##dcli $VERBOSITY secret delete
+##dcli $VERBOSITY volume create
+##dcli $VERBOSITY volume delete
