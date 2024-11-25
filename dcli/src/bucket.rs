e72d6fa02c68491a44ff7529d5d3fa04b166731b
wilbert <wilbert.schelvis@kpn.com>
2024-11-25 13:03:10 +0100

    Simplified

--- dcli/src/bucket.rs
+++ dcli/src/bucket.rs
@@ -9,8 +9,7 @@ use crate::arguments::target_argument;
 use crate::capability::{Capability, CapabilityType, CommandExecutor};
 use crate::capability_builder::CapabilityBuilder;
 use crate::flags::FlagType;
-use crate::formatters::allocation_status::{print_allocation_status, print_allocation_statuses};
-use crate::formatters::bucket::{BUCKET_LABELS, BUCKET_STATUS_LABELS};
+use crate::formatters::bucket::BUCKET_STATUS_LABELS;
 use crate::formatters::formatter::{print_vec, TableBuilder};
 use crate::subject::Subject;
 use crate::{DcliContext, DcliResult};
@@ -58,17 +57,12 @@ lazy_static! {
     CapabilityBuilder::new(CapabilityType::List, "List buckets")
       .set_long_about("Lists all available buckets.")
       .set_default_command_executor(&BucketListAll {})
-      .add_command_executors(vec![
-        (FlagType::AllocationStatus, &BucketListAllocationStatus {}, None),
-        (FlagType::Configuration, &BucketListConfiguration {}, None),
-        (FlagType::Ids, &BucketListIds {}, None),
-      ])
+      .add_command_executor(FlagType::Ids, &BucketListIds {}, None)
       .set_run_all_executors(true)
   );
   pub static ref BUCKET_SHOW_CAPABILITY: Box<(dyn Capability + Send + Sync)> = Box::new(
     CapabilityBuilder::new(CapabilityType::Show, "Show bucket configuration")
       .set_default_command_executor(&BucketShowAll {})
-      .add_command_executor(FlagType::AllocationStatus, &BucketShowAllocationStatus {}, None)
       .add_target_argument(target_argument(BUCKET_SUBJECT_TARGET, None))
   );
 }
@@ -86,50 +80,6 @@ impl CommandExecutor for BucketListAll {
     let mut builder = TableBuilder::list(&BUCKET_STATUS_LABELS, context);
     for (bucket_id, bucket_status) in bucket_ids.iter().zip(bucket_statuses) {
       builder.value(bucket_id.to_string(), &bucket_status);
-    }
-    builder.print();
-    Ok(false)
-  }
-}
-
-struct BucketListAllocationStatus {}
-
-#[async_trait]
-impl CommandExecutor for BucketListAllocationStatus {
-  async fn execute(&self, _: Option<String>, _: Option<String>, _: &ArgMatches, context: &DcliContext) -> DcliResult {
-    if context.show_capability_explanation() {
-      println!("list all buckets with their allocation status");
-    }
-    let bucket_ids = context.dsh_api_client.as_ref().unwrap().list_bucket_ids().await?;
-    let allocation_statuses = try_join_all(
-      bucket_ids
-        .iter()
-        .map(|bucket_id| context.dsh_api_client.as_ref().unwrap().get_bucket_allocation_status(bucket_id)),
-    )
-    .await?;
-    print_allocation_statuses(bucket_ids, allocation_statuses, context);
-    Ok(false)
-  }
-}
-
-struct BucketListConfiguration {}
-
-#[async_trait]
-impl CommandExecutor for BucketListConfiguration {
-  async fn execute(&self, _: Option<String>, _: Option<String>, _: &ArgMatches, context: &DcliContext) -> DcliResult {
-    if context.show_capability_explanation() {
-      println!("list all buckets with their configuration");
-    }
-    let bucket_ids = context.dsh_api_client.as_ref().unwrap().list_bucket_ids().await?;
-    let buckets = try_join_all(
-      bucket_ids
-        .iter()
-        .map(|bucket_id| context.dsh_api_client.as_ref().unwrap().get_bucket_configuration(bucket_id.as_str())),
-    )
-    .await?;
-    let mut builder = TableBuilder::list(&BUCKET_LABELS, context);
-    for (bucket_id, bucket) in bucket_ids.iter().zip(buckets) {
-      builder.value(bucket_id.to_string(), &bucket);
     }
     builder.print();
     Ok(false)
@@ -162,21 +112,6 @@ impl CommandExecutor for BucketShowAll {
     let mut builder = TableBuilder::show(&BUCKET_STATUS_LABELS, context);
     builder.value(bucket_id, &bucket);
     builder.print();
-    Ok(false)
-  }
-}
-
-struct BucketShowAllocationStatus {}
-
-#[async_trait]
-impl CommandExecutor for BucketShowAllocationStatus {
-  async fn execute(&self, target: Option<String>, _: Option<String>, _: &ArgMatches, context: &DcliContext) -> DcliResult {
-    let bucket_id = target.unwrap_or_else(|| unreachable!());
-    if context.show_capability_explanation() {
-      println!("show the allocation status for bucket '{}'", bucket_id);
-    }
-    let allocation_status = context.dsh_api_client.as_ref().unwrap().get_bucket_allocation_status(bucket_id.as_str()).await?;
-    print_allocation_status(bucket_id, allocation_status, context);
     Ok(false)
   }
 }
