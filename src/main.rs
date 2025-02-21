22000932df199be7d1edf7143a03731eabba032d
wilbert <wilbert.schelvis@kpn.com>
2025-02-21 12:55:47 +0100

    Version to 0.5.1

--- src/main.rs
+++ src/main.rs
@@ -101,7 +101,7 @@ const AFTER_HELP: &str = "For most commands adding an 's' as a postfix will yiel
    as using 'dsh app list'.";
 const USAGE: &str = "dsh [OPTIONS] [SUBJECT/COMMAND]\n       dsh --help\n       dsh secret --help\n       dsh secret list --help";
 
-const VERSION: &str = "0.5.0";
+const VERSION: &str = "0.5.1";
 
 const ENV_VAR_PREFIX: &str = "DSH_CLI_";
 
