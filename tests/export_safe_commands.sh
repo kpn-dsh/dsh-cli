e7f18af656c8d30e713afc422f1193fe50117d93
wilbert <wilbert.schelvis@kpn.com>
2025-02-25 14:01:25 +0100

    Added some tests

---
+++ tests/export_safe_commands.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+export APP_UNDER_TEST=kafdrop
+export APPLICATION_UNDER_TEST=keyring-dev
+export BUCKET_UNDER_TEST=cpr
+export CERTIFICATE_UNDER_TEST=broker
+export ENV_VALUE_UNDER_TEST=info
+export ENV_VALUE_UNDER_TEST_REGEX="^info$"
+export IMAGE_UNDER_TEST=registry:eavesdropper:0.9.2
+export IMAGE_UNDER_TEST_REGEX=registry
+export PLATFORM_UNDER_TEST=prodlz
+export PROXY_UNDER_TEST=broker
+export SECRET_UNDER_TEST=boss-account-ids
+export TENANT_UNDER_TEST=greenbox-dev
+export TOPIC_UNDER_TEST=reference-implementation-compliant
+export VENDOR_UNDER_TEST=kpn
+export VHOST_UNDER_TEST=greenbox-dev
+export VOLUME_UNDER_TEST=github-action-runner-home
+
+export SAFE_COMMANDS=(
+  "api get secret-ids"
+  # "api show"
+
+  "app list --ids"
+  "app list"
+  "app show $APP_UNDER_TEST"
+
+  "application list --ids"
+  "application list --started"
+  "application list --status"
+  "application list --stopped"
+  "application list --tasks"
+  "application list"
+  "application show $APPLICATION_UNDER_TEST --status"
+  "application show $APPLICATION_UNDER_TEST --tasks"
+  "application show $APPLICATION_UNDER_TEST"
+
+  "bucket list --ids"
+  "bucket list"
+  "bucket show $BUCKET_UNDER_TEST"
+
+  "certificate list --configuration"
+  "certificate list --ids"
+  "certificate list --status"
+  "certificate list --usage"
+  "certificate list"
+  "certificate show $CERTIFICATE_UNDER_TEST --status"
+  "certificate show $CERTIFICATE_UNDER_TEST --usage"
+  "certificate show $CERTIFICATE_UNDER_TEST"
+
+  "env find $ENV_VALUE_UNDER_TEST --started"
+  "env find $ENV_VALUE_UNDER_TEST --stopped"
+  "env find $ENV_VALUE_UNDER_TEST"
+  "env find $ENV_VALUE_UNDER_TEST_REGEX --regex --started"
+  "env find $ENV_VALUE_UNDER_TEST_REGEX --regex --stopped"
+  "env find $ENV_VALUE_UNDER_TEST_REGEX --regex"
+
+  "image find $IMAGE_UNDER_TEST --started"
+  "image find $IMAGE_UNDER_TEST --stopped"
+  "image find $IMAGE_UNDER_TEST"
+  "image find $IMAGE_UNDER_TEST_REGEX --regex --started"
+  "image find $IMAGE_UNDER_TEST_REGEX --regex --stopped"
+  "image find $IMAGE_UNDER_TEST_REGEX --regex"
+  "image list --started"
+  "image list --stopped"
+  "image list"
+
+  "metric list --started"
+  "metric list --stopped"
+  "metric list"
+
+  "platform export"
+  "platform list"
+  "platform open app $APP_UNDER_TEST --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform open app $APP_UNDER_TEST"
+  "platform open console --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform open console"
+  "platform open monitoring --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform open monitoring"
+  "platform open service $APPLICATION_UNDER_TEST --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform open service $APPLICATION_UNDER_TEST"
+  "platform open swagger --platform $PLATFORM_UNDER_TEST"
+  "platform open swagger"
+  "platform open tenant --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform open tenant"
+  "platform open tracing --platform $PLATFORM_UNDER_TEST"
+  "platform open tracing"
+  "platform show --app $APP_UNDER_TEST --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform show --app $APP_UNDER_TEST --vendor $VENDOR_UNDER_TEST --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform show --app $APP_UNDER_TEST --vendor $VENDOR_UNDER_TEST"
+  "platform show --app $APP_UNDER_TEST"
+  "platform show --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform show --service $APPLICATION_UNDER_TEST --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform show --service $APPLICATION_UNDER_TEST"
+  "platform show --vhost $VHOST_UNDER_TEST --platform $PLATFORM_UNDER_TEST --tenant $TENANT_UNDER_TEST"
+  "platform show --vhost $VHOST_UNDER_TEST"
+  "platform show"
+
+  "proxy list --ids"
+  "proxy list"
+  "proxy show $PROXY_UNDER_TEST"
+
+  "secret list --app"
+  "secret list --application"
+  "secret list --status"
+  "secret list --system"
+  "secret list --usage"
+  "secret list"
+  "secret show $SECRET_UNDER_TEST --usage"
+  "secret show $SECRET_UNDER_TEST --value"
+  "secret show $SECRET_UNDER_TEST"
+
+  "setting list"
+
+  "target list"
+
+  "topic list --ids"
+  "topic list --status"
+  "topic list --usage"
+  "topic list"
+  "topic show $TOPIC_UNDER_TEST --properties"
+  "topic show $TOPIC_UNDER_TEST --status"
+  "topic show $TOPIC_UNDER_TEST --usage"
+  "topic show $TOPIC_UNDER_TEST"
+
+  "vhost list"
+
+  "volume list --app"
+  "volume list --application"
+  "volume list --configuration"
+  "volume list --ids"
+  "volume list --status"
+  "volume list --usage"
+  "volume list"
+  "volume show $VOLUME_UNDER_TEST --status"
+  "volume show $VOLUME_UNDER_TEST --usage"
+  "volume show $VOLUME_UNDER_TEST"
+)
