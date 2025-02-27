f77267de0566d6303da86ac4b87eaf9332a1b9ad
wilbert <wilbert.schelvis@kpn.com>
2025-02-27 14:50:27 +0100

    Changed some flags

--- tests/run_all_safe_commands.sh
+++ tests/run_all_safe_commands.sh
@@ -11,21 +11,16 @@ export DSH_CLI_PASSWORD_FILE=../np-aws-lz-dsh.greenbox-dev.pwd
 
 export SEPARATOR="-------------------------------"
 
-#export MATCHING_STYLE="--matching-style bold"
 #export OUTPUT_FORMAT="--output-format json"
-#export SHOW_EXECUTION_TIME="--show-execution-time"
 #export VERBOSITY="-v high"
 #export LOG_LEVEL="--log-level debug"
 
-export DRY_RUN="--dry-run"
-
 source export_safe_commands.sh
 
-#IFS=$'\n'
 set -f
 for COMMAND in "${SAFE_COMMANDS[@]}"
 do
-  CMD=`echo "dsh $DRY_RUN $MATCHING_STYLE $OUTPUT_FORMAT $SHOW_EXECUTION_TIME $VERBOSITY $LOG_LEVEL $COMMAND" | envsubst`
+  CMD=`echo "dsh $OUTPUT_FORMAT $VERBOSITY $LOG_LEVEL $COMMAND" | envsubst`
   echo "$SEPARATOR"
   echo "$CMD"
   echo "$SEPARATOR"
