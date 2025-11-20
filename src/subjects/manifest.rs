53a9f7a4e3270a7fb7217eefe7d57cf0d2263626
wilbert <wilbert.schelvis@kpn.com>
2025-11-20 13:00:33 +0100

    Improved printing results

--- src/subjects/manifest.rs
+++ src/subjects/manifest.rs
@@ -147,18 +147,18 @@ impl CommandExecutor for ManifestExplain {
         .map_err(|_| format!("app catalog manifest '{}' does not exist", manifest_id))?,
     };
     context.print_execution_time(start_instant);
-    context.print_outcome(manifest.name);
+    context.print(manifest.name);
     if let Some(description) = manifest.description {
-      context.print_outcome(description);
+      context.print(description);
     }
     if manifest.draft {
-      context.print_warning(format!("{}:{} is a draft manifest", manifest.id, manifest.version));
+      context.print(format!("{}:{} is a draft manifest", manifest.id, manifest.version));
     } else {
-      context.print_outcome(format!("{}:{}", manifest.id, manifest.version));
+      context.print(format!("{}:{}", manifest.id, manifest.version));
     }
     if let Some(more_info) = manifest.more_info {
-      context.print_outcome("");
-      context.print_outcome(termimad::text(more_info.as_str()));
+      context.print("");
+      context.print(termimad::text(more_info.as_str()));
     }
     if let Some(configuration) = &manifest.configuration {
       let mut property_ids = configuration.properties.keys().map(|id| id.to_string()).collect_vec();
