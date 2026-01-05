0fb96c6cefbeacd32b46a5878400c828b03f0d2e
wilbert <wilbert.schelvis@kpn.com>
2026-01-05 16:17:56 +0100

    Formatting

--- src/subjects/volume.rs
+++ src/subjects/volume.rs
@@ -324,7 +324,7 @@ impl CommandExecutor for VolumeShowUsage {
     let (dependant_apps, dependant_services): (Vec<DependantApp>, Vec<DependantApplication<VolumeInjection>>) = usages.into_iter().partition_map(|dependant| match dependant {
       Dependant::App(app) => Either::Left(app),
       Dependant::Application(service) => Either::Right(service),
-      Dependant::Proxy(_) => unreachable!()
+      Dependant::Proxy(_) => unreachable!(),
     });
     if dependant_services.is_empty() {
       context.print_outcome("volume not used in services")
